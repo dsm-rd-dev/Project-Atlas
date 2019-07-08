5b5198b85632b7b12801a46acaa9e0709d8ea756
Grayton Ward <gward@dsm.net>
2019-07-08 15:45:23 -0400

    Setup development run enviroment as well as basic readme

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,43 @@ const cw = require('../connectors/cw');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 router.get('/ticket/:id', (req, res, next) => {
   cw.getTicketById(req.params.id).then(results => {
-    var info = [
-      {
+    var info = [{
         title: "ID",
         info: results.id
       },
       {
         title: "Summary",
         info: results.summary
+      },
+      {
+        title: "Contact",
+        info: results.contact.name
+      },
+      {
+        title: "Email",
+        info: results.contactEmailAddress
+      },
+      {
+        title: "Phone Number",
+        info: results.contactPhoneNumber
       }
     ]
-    res.render('report', {title: "Ticket Report", subhead: "Ticket Report: " + results.id, items: info});
-}).catch(error => {
+    res.render('report', {
+      title: "Ticket Report",
+      subhead: "Ticket Report: " + results.id,
+      items: info
+    });
+  }).catch(error => {
     res.send(error);
-});
-  
+  });
 });
 
 module.exports = router;
